581a5742da91f37aca292e4fe19a403e1023a9de
zhuang0700 <zhuang0200@126.com>
2018-01-13 18:30:47 +0800

    excel data test

--- insight/service/data_editor_service.py
+++ insight/service/data_editor_service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from admodels.models import RailwayLine
 from insight.util.railway_excel_namemap import railway_namemap
 
 __author__ = 'weiwenliang'
@@ -15,7 +16,9 @@ def parse_excel(excel_path):
     first_sheet = xls_data[sheets[0]]
     head_line = first_sheet[0]
     head_column_list = parse_head_list(head_line)
-    print(head_column_list)
+    for data_line in first_sheet[1:]:
+        railway_line = convert_data_into_railway(data_line, head_column_list)
+        print(railway_line)
 
 
 def parse_head_list(head_line):
@@ -24,6 +27,15 @@ def parse_head_list(head_line):
         head_column_name = railway_namemap.get(head, "")
         head_column_list.append(head_column_name)
     return head_column_list
+
+
+def convert_data_into_railway(data_line, head_column_list):
+    railwayLine = RailwayLine()
+    for i, data in data_line:
+        column = head_column_list[i]
+        if column:
+            railwayLine.__setattr__(column, data_line)
+    return railwayLine
 
 
 def main():
